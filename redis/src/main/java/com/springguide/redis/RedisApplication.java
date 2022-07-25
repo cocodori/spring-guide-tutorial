76e961b9073651077976ba020c1e5e04ddfa112c
cocodori <moradulee@gmail.com>
2022-07-25 22:55:24 +0900

    Messaging with Redis Tutorial:https://spring.io/guides/gs/messaging-redis/

--- redis/src/main/java/com/springguide/redis/RedisApplication.java
+++ redis/src/main/java/com/springguide/redis/RedisApplication.java
@@ -1,13 +1,69 @@
 package com.springguide.redis;
 
+import com.springguide.redis.messagingredis.Receiver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
+/**
+ * Redis Messaging Guide
+ */
 @SpringBootApplication
 public class RedisApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RedisApplication.class, args);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisApplication.class);
+
+    //컨테이너에 리스너 등록
+    @Bean
+    RedisMessageListenerContainer container(
+            RedisConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, new PatternTopic("test-pattern"));
+
+        return container;
+    }
+
+    //메시지 리스너 등록
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    @Bean
+    Receiver receiver() {
+        return new Receiver();
+    }
+
+    //메시지를 보낼 때 필요
+    @Bean
+    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationContext ctx = SpringApplication.run(RedisApplication.class);
+
+        StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+        Receiver receiver = ctx.getBean(Receiver.class);
+
+        while (receiver.getCount() < 10) {
+            LOGGER.info("Sending message...");
+            template.convertAndSend("test-pattern", "Hello FROM REDIS!!!!");
+            Thread.sleep(100L);
+        }
+
+        System.exit(0);
     }
 
 }
